b00881d99fe1d1a9607341601fb5feea89412c62
aandreasyan <aandreasyan@W10AANDRX1>
2017-01-11 15:25:34 -0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+
+    class Animal
+    {
+        static void Main(string[] args)
+        {
+            Animal an = new Animal();
+
+            List<int> numList= new List<int> {5,10,15,20,25};
+
+            List<int> oddNums = numList.Where(n => n % 2 == 1).ToList();
+
+            foreach (int num in oddNums)
+            {
+                Console.WriteLine(num + ",");
+            }
+    }
+    }
+
+
+}
